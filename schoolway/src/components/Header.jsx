1f84a5e72e8e3b9f3daf6faa60967f4dbf3a98b1
finu <finufavasp@gmail.com>
2024-06-05 12:42:35 +0530

    message

--- schoolway/src/components/Header.jsx
+++ schoolway/src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
-    
+    <Navbar className="bg-body-transparent">
+        <Container>
+          <Link to={'/'} style={{textDecoration:'none'}}>
+          <Navbar.Brand href="#home" >
+          <i className="fa-thin fa-school fa-beat-fade"  style={{color: "#FFD43B",}} />
+            {' '}
+            Schoolway
+          </Navbar.Brand>
+          </Link>
+          <Link className='btn btn-success' to={'/profile'}>
+          Profile {''}
+          </Link>
+        </Container>
+      </Navbar>
     </>
   )
 }
